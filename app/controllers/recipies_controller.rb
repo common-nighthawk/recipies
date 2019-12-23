b3480b8a4920d64dedf5527010ffa3165971a5d8
Daniel Deutsch <daniel.deutsch@outreach.io>
2019-12-23 09:35:05 -0800

    Store recipies in cookie

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -1,5 +1,8 @@
 class RecipiesController < ApplicationController
   def index
+    slug = params[:slug]
+    cookies[:slugs] = [] if cookies[:slugs].nil?
+    cookies[:slugs] << slug if slug.present?
     @recipies = Dir.glob('/home/daniel/recipes_for_erin/app/assets/recipies/*.json').sort
   end
 end
