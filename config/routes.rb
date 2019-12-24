baa53e6918d4313333c94d74a3190125a174f1da
Daniel Deutsch <daniel.deutsch@outreach.io>
2019-12-24 13:43:40 -0800

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'recipies#index'
   resources :recipies, only: [:index, :show]
   resources :ingredients, only: :index
   resources :lists, only: [:index, :create, :show]
